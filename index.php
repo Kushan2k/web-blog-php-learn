029e9c9f04391c7599a86d51024dbe6e78327699
kushan2k <kushangayantha001@gmail.com>
2022-07-18 14:15:03 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Web Blog</title>
+
+  <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+
+
+
+<div class="navbar">
+  <ul class="navbar-list">
+    <li class="navbar-links">
+      <a href="">Home</a>
+    </li>
+    <li class="navbar-links">
+      <a href="./php/profile.php">Profile</a>
+    </li>
+    <li class="navbar-links">
+      <a href="./php/post.php">Post</a>
+    </li>
+    <li class="navbar-links">
+      <a href="./php/about-us.php">About Us</a>
+    </li>
+  </ul>
+</div>
+  
+</body>
+</html>
